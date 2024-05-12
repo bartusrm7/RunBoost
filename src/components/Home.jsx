782e7d3b6832b11913e4dcc7d6ebd4b57ce44931
Bartek <bbogucki7@gmail.com>
2024-05-12 15:57:54 +0200

    finished home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
 function Home() {
 	return (
 		<>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./sass/nav-component.scss";
 import "./sass/home.scss";
 import "./sass/best-shoes-component.scss";
 import "./sass/running-shoes-finder.scss";
+import "./sass/training-calculator.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
